17b26591e8865264ebaa477a74e9e60a308aaeef
Boris the Sheep <alenasynkova@Syn.local>
2017-09-23 06:52:23 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Small challenge exercises
+ */
+public class Main {
+    public static void main(String[] srgs) {
+
+        //TODO: Write a Java program that keeps a number from the user and generates an integer between 1 and 7 and displays the name of the weekday.
+        // Comment your work!
+
+
+        
+        //TODO: Write a program in Java to input 5 numbers from keyboard and find their sum and average.
+        // Comment your work!
+
+
+
+        //TODO: Write a program in Java to display the multiplication table of a given integer.
+        //Comment your work!
+    }
+}
+
